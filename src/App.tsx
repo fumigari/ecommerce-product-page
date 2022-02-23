cd9abc5b9e1435e4ce1df4d1a899d1b7a919049a
Ariany Ferreira <arianysferreira1@gmail.com>
2022-02-23 10:01:37 -0300

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logomarca from 'src/images/logomarca.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <img src={logomarca}/>
+          <ul className="List">
+              <li className="List-item">
+                  <a href=''>Collections</a>
+              </li>
+              <li className="List-item">
+                  <a href=''>Men</a>
+              </li>
+              <li className="List-item">
+                  <a href=''>Women</a>
+              </li>
+              <li className="List-item">
+                  <a href=''>About</a>
+              </li>
+              <li className="List-item">
+                  <a href=''>Contact</a>
+              </li>
+          </ul>
+          <div>
+              <img/>
+              <img/>
+          </div>
       </header>
-    </div>
   );
 }
 
