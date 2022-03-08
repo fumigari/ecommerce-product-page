856915fe7101e0f912ddacaeb8c387fa04783d45
nataliakikuchi <37075313+nataliakikuchi@users.noreply.github.com>
2022-03-08 10:09:13 -0300

    Adds styles to the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import React from 'react';
-import logomarca from 'src/images/logomarca.svg';
-import './App.css';
+import React from "react";
+import logomarca from "./images/logomarca.svg";
+import iconCart from "./images/icon-cart.svg";
+import avatarProfile from "./images/image-avatar.png";
+import styles from "./App.module.css";
 
 function App() {
   return (
-      <header className="App-header">
-          <img src={logomarca}/>
-          <ul className="List">
-              <li className="List-item">
-                  <a href=''>Collections</a>
-              </li>
-              <li className="List-item">
-                  <a href=''>Men</a>
-              </li>
-              <li className="List-item">
-                  <a href=''>Women</a>
-              </li>
-              <li className="List-item">
-                  <a href=''>About</a>
-              </li>
-              <li className="List-item">
-                  <a href=''>Contact</a>
-              </li>
-          </ul>
-          <div>
-              <img/>
-              <img/>
-          </div>
-      </header>
+    <header className={styles.appHeader}>
+      <img src={logomarca} alt="sneakers logo" />
+      <ul className={styles.list}>
+        <li className={styles.listItem}>
+          <a href="">Collections</a>
+        </li>
+        <li className={styles.listItem}>
+          <a href="">Men</a>
+        </li>
+        <li className={styles.listItem}>
+          <a href="">Women</a>
+        </li>
+        <li className={styles.listItem}>
+          <a href="">About</a>
+        </li>
+        <li className={styles.listItem}>
+          <a href="">Contact</a>
+        </li>
+        <li>
+          <a>
+            <img src={iconCart} alt="ícone carrinho de compras" />
+          </a>
+        </li>
+        <li>
+          <a>
+            <img src={avatarProfile} alt="imagem de perfil da usuária" />
+          </a>
+        </li>
+        <li></li>
+      </ul>
+    </header>
   );
 }
 
