e5d6b10cb386d84e83bfc9195b4c0c8f16c54261
nataliakikuchi <37075313+nataliakikuchi@users.noreply.github.com>
2022-03-16 10:27:33 -0300

    WIP - tests

--- src/components/header/components/item/index.spec.tsx
+++ src/components/header/components/item/index.spec.tsx
@@ -1,5 +1,11 @@
+import { render } from "@testing-library/react";
+import Item from ".";
+
 describe("<Item />", () => {
   test("render item", () => {
-    render(<Item />);
+    const { container } = render(<Item />);
+    const result = container.querySelector("li");
+
+    expect(result?.textContent).toBe("");
   });
 });
