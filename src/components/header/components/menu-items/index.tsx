9f4d3d1f5469faa2e6ee0649e91a5750ad54a42b
BrunoAST <bruno-alves2010@live.com>
2022-03-22 09:51:55 -0300

    feat: create menu-items component

--- src/components/header/components/menu-items/index.tsx
+++ src/components/header/components/menu-items/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "./index.css";
 
-const MenuItems = () => {
-  return <div />;
+const MenuItems: React.FC = (): JSX.Element => {
+  return (
+    <ul role="list" className="menu">
+      <li className="menu__items">Collections</li>
+      <li className="menu__items">Men</li>
+      <li className="menu__items">Women</li>
+      <li className="menu__items">About</li>
+      <li className="menu__items">Contact</li>
+    </ul>
+  );
 };
 
 export default MenuItems;
