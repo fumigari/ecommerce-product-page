05bcca26b963a38591bf787db15b9442e97f4543
BrunoAST <bruno-alves2010@live.com>
2022-03-22 09:53:00 -0300

    test: create tests for menu-items component

--- src/components/header/components/menu-items/index.spec.tsx
+++ src/components/header/components/menu-items/index.spec.tsx
@@ -1,11 +1,46 @@
-import { render } from "@testing-library/react";
-import Item from ".";
+import { render, screen } from "@testing-library/react";
+import MenuItems from ".";
 
 describe("<MenuItems />", () => {
-  test("render menu-items", () => {
-    const { container } = render(<Item />);
-    const result = container.querySelector("li");
+  test("should render menu-items", () => {
+    render(<MenuItems />);
+    const result = screen.queryByRole("list");
+    expect(result).toBeInTheDocument();
+  });
 
-    expect(result?.textContent).toBe("");
+  test("should contain 5 list items", () => {
+    render(<MenuItems />);
+    const result = screen.getByRole("list");
+    expect(result.childElementCount).toBe(5);
+  });
+
+  test("should have 'Collections' menu item", () => {
+    render(<MenuItems />);
+    const result = screen.getByText("Collections");
+    expect(result).toBeInTheDocument();
+  });
+
+  test("should have 'Men' menu item", () => {
+    render(<MenuItems />);
+    const result = screen.getByText("Men");
+    expect(result).toBeInTheDocument();
+  });
+
+  test("should have 'Women' menu item", () => {
+    render(<MenuItems />);
+    const result = screen.getByText("Women");
+    expect(result).toBeInTheDocument();
+  });
+
+  test("should have 'About' menu item", () => {
+    render(<MenuItems />);
+    const result = screen.getByText("About");
+    expect(result).toBeInTheDocument();
+  });
+
+  test("should have 'Contact' menu item", () => {
+    render(<MenuItems />);
+    const result = screen.getByText("Contact");
+    expect(result).toBeInTheDocument();
   });
 });
