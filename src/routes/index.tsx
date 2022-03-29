49f5b58187c81ad70827a36efa3b7207ce101bcf
nataliakikuchi <natalia.kikuchi@thoughtworks.com>
2022-03-29 10:36:52 -0300

    feat: creates routes file

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const PageRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path= {} element= {}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default PageRoutes;
