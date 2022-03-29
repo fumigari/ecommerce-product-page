9be3f2bdda8da690722795f11c143ad4bd72e991
nataliakikuchi <natalia.kikuchi@thoughtworks.com>
2022-03-29 13:24:53 -0300

    chore: includes comments to explain React Router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const PageRoutes = () => {
   return (
@@ -11,3 +11,7 @@ const PageRoutes = () => {
 }
 
 export default PageRoutes;
+
+// React Router: lib para controle de rotas que permite configurar as rotas utilizando o formato JSX.
+// BrowserRouter: componente que informar à aplicação que teremos um roteamento de componentes, por isso fica em volta dos componentes <Route>.
+// Route: associa a rota ao componente.
