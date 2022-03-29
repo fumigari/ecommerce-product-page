a28786913702c893442a8cea376e2039ba1c6a5b
Ariany Ferreira <arianysferreira1@gmail.com>
2022-03-29 10:07:06 -0300

    refactor: add cart component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,31 +1,15 @@
 import React from "react";
-import iconCart from "../../images/icon-cart.svg";
-import avatarProfile from "../../images/image-avatar.png";
 import styles from "./index.module.css";
 import MenuItems from './components/menu-items';
 import Logo from "./components/logo";
+import Cart from "./components/cart";
 
 const Header = () => {
   return (
     <header className={styles.appHeader}>
       <Logo />
       <MenuItems />
-      <ul className={`${styles.listWrapper} ${styles.cartAndIcon}`}>
-        <li>
-          <a>
-            <img src={iconCart} alt="ícone carrinho de compras" />
-          </a>
-        </li>
-        <li>
-          <a>
-            <img
-              src={avatarProfile}
-              alt="imagem de perfil da usuária"
-              className={styles.avatar}
-            />
-          </a>
-        </li>
-      </ul>
+      <Cart />
     </header>
   );
 };
