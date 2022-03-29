af3104f6bbf4544a5d1e1067e81c206bc2cbc515
Ariany Ferreira <arianysferreira1@gmail.com>
2022-03-29 09:33:11 -0300

    refactor: add logo component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import logomarca from "../../images/logomarca.svg";
 import iconCart from "../../images/icon-cart.svg";
 import avatarProfile from "../../images/image-avatar.png";
 import styles from "./index.module.css";
 import MenuItems from './components/menu-items';
+import Logo from "./components/logo";
 
 const Header = () => {
   return (
     <header className={styles.appHeader}>
-      <img src={logomarca} alt="sneakers logo" />
+      <Logo />
       <MenuItems />
       <ul className={`${styles.listWrapper} ${styles.cartAndIcon}`}>
         <li>
