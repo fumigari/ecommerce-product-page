0b193e0282a13cbcd2bd2aedd252a1f2e0f8fc8f
Ariany Ferreira <arianysferreira1@gmail.com>
2022-04-19 10:20:46 -0300

    feat: create NotFound component

---
+++ src/pages/not-found/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import NotFoundImage from "../../images/not-found.svg";
+import "./index.css";
+
+const NotFound: React.FC = () => {
+    return <div className="not-found"><img src={NotFoundImage} alt="page not found" /></div>
+}
+
+export default NotFound;
