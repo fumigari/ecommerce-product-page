78fccd7b581235ce7e6a66b375b95a724e17c7e7
Ariany Ferreira <arianysferreira1@gmail.com>
2022-05-04 09:54:37 -0300

    refactor: update Cart component

--- src/components/header/components/cart/index.tsx
+++ src/components/header/components/cart/index.tsx
@@ -2,21 +2,20 @@ import React from "react";
 import "./index.css";
 import avatarProfile from "../../../../images/image-avatar.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
 const Cart: React.FC = (): JSX.Element => {
-  return (<ul role="list" className="list">
-    <li>
-      <FontAwesomeIcon icon={faCartShopping} size="2x" />
-    </li>
-    <li>
-      <img
-        src={avatarProfile}
-        alt="imagem de perfil da usuária"
-        className="list__avatar"
-      />
-    </li>
-  </ul>);
-}
+  return (
+    <ul role="list" className="list">
+      <li role="menuitem">
+        <FontAwesomeIcon icon={faCartShopping} size="2x" />
+      </li>
+      <li role="menuitem">
+        <FontAwesomeIcon icon={faCircleUser} size="2x" />
+      </li>
+    </ul>
+  );
+};
 
 export default Cart;
